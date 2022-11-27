f0772e4cfe0245d5f87f8a20b2b1767d873701f5
arjunnarang2020 <83542729+arjunnarang2020@users.noreply.github.com>
2022-11-27 11:52:35 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,22 @@
+function send(){
+    msg = document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({
+        name: user_name;
+        message: msg, 
+        like: 0
+    });
+    document.getElementById("msg").value = "";
+}
+
+function redirectToRoom(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "kwitter.html";
+}
